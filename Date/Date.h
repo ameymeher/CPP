fb3fe1d9e67c87d95f7daca2ca6bdc80a3cda8b1
Amey <amey.meher@somaiya.edu>
2019-05-14 12:56:29 +0530

    Date project started

--- Date/Date.h
+++ Date/Date.h
@@ -5,12 +5,55 @@
 #ifndef SAMPLEPROJECT_DATE_H
 #define SAMPLEPROJECT_DATE_H
 
-
 class Date {
+
+    using UI = unsigned int;
+
     private:
-        unsigned int day;
-        unsigned int month;
-        unsigned int year
+        UI day;
+        UI month;
+        UI year;
+
+    public:
+
+        Date(UI day, UI month, UI year){
+
+            if(month >=1 && month <=12)
+                this->month = month;
+            else
+                throw "Month invalid";
+
+            if(month )
+            this->day = day;
+
+            this->year = year;
+        }
+
+        UI getDay() const {
+            return day;
+        }
+
+        void setDay(UI day) {
+            Date::day = day;
+        }
+
+        UI getMonth() const {
+            return month;
+        }
+
+        void setMonth(UI month) {
+            Date::month = month;
+        }
+
+        UI getYear() const {
+            return year;
+        }
+
+        void setYear(UI year) {
+            Date::year = year;
+        }
+
+
 };
 
 
